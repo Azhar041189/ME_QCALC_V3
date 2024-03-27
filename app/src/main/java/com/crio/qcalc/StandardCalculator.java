4df31d7e98810651eb37508a227efad47a74aec9
CrioUser <crio-user@criodo.com>
2024-03-27 20:11:11 +0530

    ME_QCALC_V3_MODULE_DEBUGGING

--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -1,12 +1,52 @@
 package com.crio.qcalc;
 
 public class StandardCalculator {
-
-    public static void getVersion(){
+    
+   public static void getVersion(){
         System.out.println("Standard Calculator 1.0");
     }
+    private int result;
+    public int getResult() {
+        return result;
+         }
+    private void setResult(int value) {
+        this.result = value;
+     }
+ 
+    public void add(int num1, int num2) {
+    result = num1 + num2;
+    setResult(result);
+    }
+
+    public void subtract(int num1, int num2) {
+    result = num1 - num2;
+    setResult(result);
+    }
+
+    public void multiply(int num1, int num2) {
+    result = num1 * num2;
+    setResult(result);
+    }
+
+    public void divide(int num1, int num2) {
+    if (num2 == 0) {
+        throw new IllegalArgumentException("Cannot divide by zero");
+    }
+    result = num1 / num2;
+    setResult(result);
+    }
+    
+    public void clearResult() {
+
+    result = 0;
+    }
+    
+public void printResult(){
+
+    System.out.println("Standard Calculator Result:"+ getResult());
+
+}
 
 
-
-
+   
 }
